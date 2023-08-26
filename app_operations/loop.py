93b6600d07dd3799d05d684586a04a73c69c5f99
Victorcho_tf <117527071+victorchoTF@users.noreply.github.com>
2023-08-27 01:53:04 +0300

    Add loop.py

---
+++ app_operations/loop.py
@@ -0,0 +1,22 @@
+from typing import List
+from pygame import display, event, quit as pg_quit
+from app_operations.gen_surf_rect_list import gen_surf_rect_list
+from app_operations.quit_loop import quit_loop
+from app_operations.restart import restart
+from screen import screen
+
+
+def loop(nums: List[int], idx: int = -1, second_idx: int = -1, pivot: int = -1) -> None:
+    for e in event.get():
+        if quit_loop(e):
+            pg_quit()
+            exit()
+        if restart(e):
+            raise StopIteration
+
+    screen.fill("#DEBA69")
+    surf_rect_array = gen_surf_rect_list(nums, idx, second_idx, pivot)
+    for surf, rect in surf_rect_array:
+        screen.blit(surf, rect)
+
+    display.update()
