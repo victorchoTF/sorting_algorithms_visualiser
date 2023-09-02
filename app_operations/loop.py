05e4e0e19d5b01dd66e93b7c19aa1fed872d4b0c
Victorcho_tf <117527071+victorchoTF@users.noreply.github.com>
2023-09-02 12:56:41 +0300

    Removing redundant imports

--- app_operations/loop.py
+++ app_operations/loop.py
@@ -1,12 +1,14 @@
 from typing import List
 from pygame import display, event, quit as pg_quit
-from app_operations.gen_surf_rect_list import gen_surf_rect_list
-from app_operations.quit_loop import quit_loop
-from app_operations.restart import restart
+from gen_surf_rect_list import gen_surf_rect_list
+from quit_loop import quit_loop
+from restart import restart
 from screen import screen
 
 
 def loop(nums: List[int], idx: int = -1, second_idx: int = -1, pivot: int = -1) -> None:
+    # NOTE: Add clock with said FPS if your computer can't handle the heat
+
     for e in event.get():
         if quit_loop(e):
             pg_quit()
