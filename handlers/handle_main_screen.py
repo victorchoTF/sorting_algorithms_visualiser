49c0872a0af5dafbc8efa2fd78bc8bab6aeb3b5f
Victorcho_tf <117527071+victorchoTF@users.noreply.github.com>
2023-09-30 22:50:18 +0300

    Adding Chose Length Option

--- handlers/handle_main_screen.py
+++ handlers/handle_main_screen.py
@@ -1,13 +1,14 @@
 from typing import List, Tuple
 from pygame import Surface, Rect
 from handlers.app_mode import AppMode
-from main_screen import MainScreen
+from screens.main_screen import MainScreen
 
 
 def handle_main_screen(main_screen: MainScreen, num_list: List[int],
                        surf_rect_list: List[Tuple[Surface, Rect]]) -> Tuple[AppMode, List[int],
                                                                             List[Tuple[Surface, Rect]]]:
     main_screen.draw(surf_rect_list)
-    num_list, surf_rect_list = main_screen.buttons_check(num_list, surf_rect_list)
+    num_list, surf_rect_list = main_screen.sorting_menu.buttons_check(num_list, surf_rect_list)
+    num_list, surf_rect_list = main_screen.list_size_menu.buttons_check(num_list, surf_rect_list)
 
     return AppMode.MAIN_SCREEN, num_list, surf_rect_list
