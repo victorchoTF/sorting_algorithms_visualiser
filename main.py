9c6a3cbfb1593a7b97b40209baee890b259b6f8c
Victorcho_tf <117527071+victorchoTF@users.noreply.github.com>
2023-10-02 18:46:55 +0300

    Updated main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from typing import List, Tuple
 from pygame import display, quit, Surface, Rect
+
+from handlers.handle_presentation_screen import handle_presentation_screen
 from screens.list_data import ListData
 from screens.main_screen import MainScreen
+from screens.presentation_screen import PresentationScreen
 from screens.screen_data import screen, clock
 from screens.title_screen import TitleScreen
 from app_operations.gen_surf_rect_list import gen_surf_rect_list
@@ -15,6 +18,7 @@ from handlers.handle_transition import handle_transition
 def main() -> None:
     title_screen: TitleScreen = TitleScreen()
     main_screen: MainScreen = MainScreen()
+    presentation_screen: PresentationScreen = PresentationScreen()
 
     num_list: List[int] = [num for num in range(1, ListData.list_size)]
     surf_rect_list: List[Tuple[Surface, Rect]] = gen_surf_rect_list(num_list)
@@ -23,6 +27,7 @@ def main() -> None:
     mode_handlers = {
         AppMode.TITLE_SCREEN: lambda: handle_title_screen(title_screen),
         AppMode.MAIN_SCREEN: lambda: handle_main_screen(main_screen, num_list, surf_rect_list),
+        AppMode.PRESENTATION_SCREEN: lambda: handle_presentation_screen(presentation_screen),
         AppMode.TRANSITION: lambda: handle_transition(title_screen, main_screen, surf_rect_list),
     }
 
@@ -31,7 +36,8 @@ def main() -> None:
         clock.tick(60)
         screen.fill("#DEBA69")
 
-        run = handle_events()
+        run, data = handle_events()
+        current_mode = data if data else current_mode
 
         current_mode, *data = mode_handlers[current_mode]()
         num_list, surf_rect_list = data if data[0] is not None else (num_list, surf_rect_list)
